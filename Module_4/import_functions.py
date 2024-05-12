e2ea68443afa6c50f2c91ecb83d601e91d966983
holite <144428331+32456543@users.noreply.github.com>
2024-05-12 11:57:20 +0300

    b

--- Module_4/import_functions.py
+++ Module_4/import_functions.py
@@ -1,3 +1,18 @@
-from modules import hi as func2 , hello as func1
-func1()
-func2()
+#1
+# from modules import hi as func2 , hello as func1
+# func1()
+# func2()
+
+
+#2
+# from modules import *
+# 
+# hi()
+# hello()
+
+
+
+#3
+# import modules
+# modules.hi()
+# modules.hello()
